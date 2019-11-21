9551bc92b74a0e31ee58e6eb96134f1139532031
Carlos Rodriguez <carlos.rodriguez@nclouds.com>
2019-11-21 00:48:31 -0600

    add resolution dropdown

--- App.js
+++ App.js
@@ -1,46 +1,61 @@
-import React, { useState } from 'react';
+import React, { useState, Component } from 'react';
 import Video from 'react-native-video';
 import VideoPlayer from 'react-native-video-controls';
+import { Dropdown } from 'react-native-material-dropdown';
+
 import {
   StyleSheet,
   Text,
   View,
-  TextInput,
-  Button,
-  ScrollView,
-  FlatList
+  Button
 } from 'react-native';
+
+
+
 export default function App() {
-  const [courseGoals, setCourseGoals] = useState([]);
-  const addGoalHandler = goalTitle => {
-    setCourseGoals(currentGoals => [
-      ...currentGoals,
-      { id: Math.random().toString(), value: goalTitle }
-    ]);
-  };
-  const onBuffer = onBuffer => {
-  };
-  const videoError = onBuffer => {
-  };
-  const var1 = "";
+
+  let data = [{
+      value: '360p',
+    }, {
+      value: '480p',
+    }, {
+      value: '720p',
+    }, {
+      value: '1080p'
+    }];
+
+  let selected_resolution = '720p'
+
   return (
     <View style={{flex: 1}}>
-      <VideoPlayer
-        style={styles.backgroundVideo}
-        source={{ uri: 'https://trambo-video.s3.amazonaws.com/video.mp4' }}
-      />
-    </View>
+        <View style={{flex: 1, backgroundColor: 'powderblue'}} >
+          <Text>TramboTube Premium</Text>
+        </View>
+        <View style={{flex: 7, backgroundColor: 'skyblue'}} >
+        <VideoPlayer
+          style={styles.backgroundVideo}
+          source={{ uri: 'https://d295x101df47z9.cloudfront.net/bmx/bmx-'+selected_resolution+'.mp4' }}
+        />
+        </View>
+        <View style={{flex: 2, backgroundColor: 'powderblue'}} >
+        <Dropdown
+          label='Resolution'
+          data={data}
+        />
+        </View>
+      </View>
   );
 }
+
 var styles = StyleSheet.create({
   backgroundVideo: {
-    position: 'absolute',
+    position: 'relative',
     top: 0,
     left: 0,
     bottom: 0,
     right: 0,
   },
   screen: {
-      padding: 50
+      padding: 100
     },
 });
